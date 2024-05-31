1b0d96744d5b5fa1f955023bf3d553697a253526
Meetpatel110503 <meetpa@zignuts.com>
2024-05-31 18:47:41 +0530

    Implement Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,33 +6,60 @@ import {
   Button,
   Box,
   IconButton,
+  Menu,
+  MenuItem,
 } from "@mui/material"
 import InstagramIcon from "@mui/icons-material/Instagram"
+import MenuIcon from "@mui/icons-material/Menu"
 import { Link, useNavigate } from "react-router-dom"
 import { UserContext } from "../context/userContext"
+import { useState } from "react"
 
 const Navbar = () => {
   const { isLoggedIn, logoutUser } = useContext(UserContext)
-
   const navigate = useNavigate()
+  const [anchorEl, setAnchorEl] = useState(null)
 
   const handleLogout = () => {
     logoutUser()
     navigate("/login")
   }
 
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <AppBar position='sticky '>
+    <AppBar position='sticky'>
       <Toolbar>
-        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-          <Link to='/' style={{ textDecoration: "none", color: "inherit" }}>
-            <IconButton color='inherit' edge='start'>
+        <Box sx={{ display: "flex", flexGrow: 1, alignItems: "center" }}>
+          <Link
+            to='/'
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <IconButton sx={{ color: "#064F57 " }} edge='start'>
               <InstagramIcon />
-            </IconButton>{" "}
-            MY POSTS
+            </IconButton>
+            <Typography
+              variant='h6'
+              component='div'
+              sx={{ fontFamily: "Proxima Nova" }}
+            >
+              MY <span style={{ color: "#064F57 " }}>POSTS</span>
+            </Typography>
           </Link>
-        </Typography>
-        <Box sx={{ flexGrow: 1 }}>
+        </Box>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
           {isLoggedIn ? (
             <>
               <Button color='inherit' onClick={handleLogout}>
@@ -75,6 +102,57 @@ const Navbar = () => {
               </Button>
             </>
           )}
+        </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <IconButton color='inherit' onClick={handleMenuOpen}>
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            {isLoggedIn ? (
+              <>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link
+                    to='/posts'
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    All Posts
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link
+                    to='/users'
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    Users
+                  </Link>
+                </MenuItem>
+              </>
+            ) : (
+              <>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link
+                    to='/login'
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    Login
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link
+                    to='/signup'
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    Signup
+                  </Link>
+                </MenuItem>
+              </>
+            )}
+          </Menu>
         </Box>
       </Toolbar>
     </AppBar>
--- src/layout/Landing.js
+++ src/layout/Landing.js
@@ -1,28 +1,43 @@
 import React from "react"
-import { Container, Typography, Box, Button } from "@mui/material"
+import { Container, Typography, Box, Button, Fade } from "@mui/material"
 import { Link } from "react-router-dom"
 
 export default function LandingPage() {
   return (
     <Container maxWidth='lg'>
-      <Box sx={{ textAlign: "center", mt: 30 }}>
-        <Typography variant='h3' component='h1' gutterBottom>
-          Welcome to the Post Management System
-        </Typography>
-        <Typography variant='h5' component='p' gutterBottom>
-          Manage your posts efficiently and effectively
-        </Typography>
+      <Box sx={{ textAlign: "center", mt: 10 }}>
+        <Fade in={true} timeout={2000}>
+          <Box>
+            <img
+              src='./assets/post.png'
+              alt='Landing'
+              style={{ maxWidth: "40%", height: "auto", borderRadius: 8 }}
+            />
+          </Box>
+        </Fade>
+        <Fade in={true} timeout={2500}>
+          <Typography variant='h3' component='h1' gutterBottom>
+            Welcome to the Post Management System
+          </Typography>
+        </Fade>
+        <Fade in={true} timeout={2000}>
+          <Typography variant='h5' component='p' gutterBottom>
+            Manage your posts efficiently and effectively
+          </Typography>
+        </Fade>
         <Box sx={{ mt: 4 }}>
-          <Button
-            variant='contained'
-            color='primary'
-            size='large'
-            component={Link}
-            to='/posts'
-            sx={{ mx: 1 }}
-          >
-            Get Started
-          </Button>
+          <Fade in={true} timeout={1500}>
+            <Button
+              variant='contained'
+              color='primary'
+              size='large'
+              component={Link}
+              to='/posts'
+              sx={{ mx: 1 }}
+            >
+              Get Started
+            </Button>
+          </Fade>
         </Box>
       </Box>
     </Container>
